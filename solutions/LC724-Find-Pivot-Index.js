d9cc6443b3e28a7fae9268f6baac41e280d04a52
Ming Ng <69211773+redconOne@users.noreply.github.com>
2022-10-07 08:53:43 -0600

    update: added instructions

--- solutions/LC724-Find-Pivot-Index.js
+++ solutions/LC724-Find-Pivot-Index.js
@@ -9,3 +9,10 @@ const pivotIndex = (nums) => {
   }
   return -1;
 };
+
+// for position 0, left will always be valued at 0, and right as the slice of string from idx 1 onwards
+// if equal return for idx 0
+// otherwise, as we iterate through the array we add the previous value to left
+// we also subtract the current value (the pivot point) from right
+// if left === right return current index
+// if we make it through the entire array there was no pivot point, so return -1
