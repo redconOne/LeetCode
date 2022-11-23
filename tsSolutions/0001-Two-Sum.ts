fb82ea509d5372d52d7270505e988009608985db
redconOne <mingleeng@gmail.com>
2022-11-22 19:03:15 -0700

    add: solution for LC-0001

---
+++ tsSolutions/0001-Two-Sum.ts
@@ -0,0 +1,16 @@
+const twoSum = (nums: number[], target: number): number[] => {
+  const map = new Map<number, number>();
+
+  for (let i: number = 0; i < nums.length; ++i) {
+    const current = nums[i],
+      comp: number = target - current;
+
+    if (map.get(comp) !== undefined) return [map.get(comp) || 0, i];
+
+    map.set(current, i);
+  }
+
+  return [];
+};
+
+export default twoSum;
