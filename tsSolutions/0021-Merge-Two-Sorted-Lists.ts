7df368835301f24b8dc32b5bd85a1b3831348c21
redconOne <mingleeng@gmail.com>
2022-11-27 11:59:35 -0700

    add: solution for LC-0021

---
+++ tsSolutions/0021-Merge-Two-Sorted-Lists.ts
@@ -0,0 +1,34 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+const mergeTwoLists = (
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null => {
+  const tempNode = new ListNode();
+  let current = tempNode;
+
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  if (list1) current.next = list1;
+  if (list2) current.next = list2;
+
+  return tempNode.next;
+};
+
+export default mergeTwoLists;
